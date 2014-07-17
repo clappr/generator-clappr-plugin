9a675272fbe29cc95913519e890a46c4372f5a9c
thiagopnts <thiagopnts@gmail.com>
2014-07-17 16:20:04 -0300

    add a default render implementation

--- app/templates/_index.js
+++ app/templates/_index.js
@@ -3,6 +3,12 @@ var JST = require('./jst');
 
 class <%= pluginName %> extends <%= className %> {
   get name() { return '<%= name %>'; }
+
+  render() {
+    var style = $('<style>').html(JST.CSS[<%= name %>]());
+    this.$el.append(style);
+    return this;
+  }
 }
 
 module.exports = window.<%= pluginName %> = <%= pluginName %>;
