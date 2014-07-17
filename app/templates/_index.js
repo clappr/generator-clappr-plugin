25e55d4f31880c772405d3c278a415c87ddcf3f2
thiagopnts <thiagopnts@gmail.com>
2014-07-17 14:26:11 -0300

    add jst and name attribute to the base index.js

--- app/templates/_index.js
+++ app/templates/_index.js
@@ -1,6 +1,8 @@
 var <%= className %> = require('<%= dependency %>');
+var JST = require('./jst');
 
 class <%= pluginName %> extends <%= className %> {
+  get name() { return '<%= name %>'; }
 }
 
 module.exports = window.<%= pluginName %> = <%= pluginName %>;
