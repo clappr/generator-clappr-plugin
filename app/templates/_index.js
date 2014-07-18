c3712be3b11c2d4572f12c81dbb61cd8891c2fc7
thiagopnts <thiagopnts@gmail.com>
2014-07-18 17:51:19 -0300

    remove broken call

--- app/templates/_index.js
+++ app/templates/_index.js
@@ -5,7 +5,7 @@ class <%= pluginName %> extends <%= className %> {
   get name() { return '<%= name %>'; }
 
   render() {
-    var style = $('<style>').html(JST.CSS[<%= name %>]());
+    var style = $('<style>').html(JST.CSS[<%= name %>]);
     this.$el.append(style);
     return this;
   }
